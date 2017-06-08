43511bb04cf3c448f81b047a11f72fc54108713a
Yoshiki Shibata <yshibata@ca2.so-net.ne.jp>
2017-06-08 14:33:56 +0900

    sync

---
+++ src/aitc/geometry.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+type Point struct{ X, Y float64 }
+
+func Distance(p, q Point) float64 { // 関数
+	return math.Hypot(q.X-p.X, q.Y-p.Y)
+}
+
+func (p Point) Distance(q Point) float64 { // メソッド
+	return math.Hypot(q.X-p.X, q.Y-p.Y)
+}
+
+func main() {
+	p := Point{1, 2}
+	q := Point{4, 6}
+	fmt.Println(Distance(p, q))
+	fmt.Println(p.Distance(q))
+}
