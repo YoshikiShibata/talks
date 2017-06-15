8d93e207da23f1fb0c684dc8ccb84f8b2f24735d
Yoshiki Shibata <yshibata@ca2.so-net.ne.jp>
2017-06-15 14:21:27 +0900

    updated

---
+++ src/aitc/sieve.go
@@ -0,0 +1,39 @@
+// A concurrent prime sieve
+
+package main
+
+import "fmt"
+
+// START OMIT
+// Send the sequence 2, 3, 4, ... to channel 'ch'.
+func Generate(ch chan<- int) { // HL
+	for i := 2; ; i++ {
+		ch <- i // Send 'i' to channel 'ch'. // HL
+	}
+}
+
+// Copy the values from channel 'in' to channel 'out',
+// removing those divisible by 'prime'.
+func Filter(in <-chan int, out chan<- int, prime int) { // HL
+	for {
+		i := <-in // Receive value from 'in'. // HL
+		if i%prime != 0 {
+			out <- i // Send 'i' to 'out'.
+		}
+	}
+}
+
+// END OMIT
+
+// The prime sieve: Daisy-chain Filter processes.
+func main() {
+	ch := make(chan int) // Create a new channel.
+	go Generate(ch)      // Launch Generate goroutine.
+	for {
+		prime := <-ch
+		fmt.Println(prime)
+		ch1 := make(chan int)
+		go Filter(ch, ch1, prime)
+		ch = ch1
+	}
+}
